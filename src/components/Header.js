14286001962d31ff23bad3dff281deb20f589762
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2021-05-31 05:40:29 -0400

    Header, Home & Section components finished also hamburger menu added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 
 const Header = () => {
+  const [burgerStatus, setBurgerStatus] = useState(false);
   return (
     <Container>
       <a href='https://www.tesla.com/' target='_blank' rel='noreferrer'>
@@ -16,8 +18,45 @@ const Header = () => {
       <RightMenu>
         <a href='#'>Shop</a>
         <a href='#'>Tesla Account</a>
-        <MenuIconContainer></MenuIconContainer>
+        <MenuIconContainer onClick={() => setBurgerStatus(true)}>
+          <MenuIcon />
+        </MenuIconContainer>
       </RightMenu>
+      <BurgerNav status={burgerStatus}>
+        <CloseContainer>
+          <Close onClick={() => setBurgerStatus(false)} />
+        </CloseContainer>
+
+        <ul>
+          <li>
+            <a href='#'>Model S</a>
+          </li>
+          <li>
+            <a href='#'>Model Y</a>
+          </li>
+          <li>
+            <a href='#'>Existing inventory</a>
+          </li>
+          <li>
+            <a href='#'>Used inventory</a>
+          </li>
+          <li>
+            <a href='#'>Trade-in</a>
+          </li>
+          <li>
+            <a href='#'>Cybertruck</a>
+          </li>
+          <li>
+            <a href='#'>Roadster</a>
+          </li>
+          <li>
+            <a href='#'>Semi</a>
+          </li>
+          <li>
+            <a href='#'>Charging</a>
+          </li>
+        </ul>
+      </BurgerNav>
     </Container>
   );
 };
@@ -30,6 +69,11 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 0 20px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 10;
 `;
 
 const MenuGroup = styled.div`
@@ -44,6 +88,7 @@ const MenuGroup = styled.div`
 
 const RightMenu = styled.div`
   display: flex;
+  align-items: center;
   a {
     font-weight: 600;
     text-transform: uppercase;
@@ -51,4 +96,40 @@ const RightMenu = styled.div`
   }
 `;
 
-const MenuIconContainer = styled.div``;
+const MenuIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Close = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  width: 300px;
+  background: white;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+  transform: ${(props) =>
+    props.status ? 'translateX(0)' : 'translateX(100%)'};
+  transition: transform 0.3s ease-in;
+  ul {
+    list-style: none;
+  }
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+    a {
+      font-weight: 600;
+    }
+  }
+`;
